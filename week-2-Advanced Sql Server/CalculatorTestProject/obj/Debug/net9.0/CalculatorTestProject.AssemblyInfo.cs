aaa89e8cc6cad5a05620f8460f718d336e840cbb
SuryatejaAdaveni <asurya2022@gmail.com>
2025-07-26 18:31:44 +0530

    Week-6 React

--- week-2-Advanced Sql Server/CalculatorTestProject/obj/Debug/net9.0/CalculatorTestProject.AssemblyInfo.cs
+++ week-2-Advanced Sql Server/CalculatorTestProject/obj/Debug/net9.0/CalculatorTestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CalculatorTestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+415b7f273200ab58f985c0d863eb855ea3866259")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aad4bed5a2434ede14e847cd5c9ec565997017b4")]
 [assembly: System.Reflection.AssemblyProductAttribute("CalculatorTestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CalculatorTestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
